cc40cc132450d12cdfc849fdd693387d55c2afcc
Jason Waldrip <jason@waldrip.net>
2013-06-01 16:40:35 -0600

    activity methods

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,8 @@
 class Activity < ActiveRecord::Base
 
-  default_scope ->{ order(:updated_at) }
+  attr_reader :status
+
+  default_scope -> { order(:updated_at) }
 
   belongs_to :device
   belongs_to :user
@@ -11,6 +13,46 @@ class Activity < ActiveRecord::Base
   has_one :return_area, as: :locatable, class_name: 'Location'
 
   after_create :set_user_from_device
+
+  def device_has_returned?
+    return_area.contains_device? device
+  end
+
+  def duration=(hours)
+    self.end_time = Time.now + hours.hours
+  end
+
+  def in_return_time_range?
+    return_time_range.cover? Time.now
+  end
+
+  def in_progress?
+    Time.now <= return_time_range.last
+  end
+
+  def status
+    case
+    when completed?
+      :completed
+    when in_return_time_range? && device_has_returned?
+      update_attributes completed: true
+      :completed
+    when in_progress?
+      :in_progress
+    else
+      :lost
+    end
+  end
+
+  def return_time_range
+    start_time = end_time - 1.hour
+    end_time   = end_time + 1.hour
+    start_time..end_time
+  end
+
+  def time_has_elapsed?
+    !in_progress?
+  end
 
   private
 
