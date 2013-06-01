766c517866622f82297d197a4dbf78f2c62f41ab
Jason Waldrip <jason@waldrip.net>
2013-06-01 10:49:55 -0600

    uuid

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130601164437) do
+ActiveRecord::Schema.define(version: 20130510030001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
-
-  create_table "activities", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
