3c2eac4c55e5c9b428fa2b20214767ce4c996750
Jason Waldrip <jason@waldrip.net>
2013-06-01 11:51:27 -0600

    change activities

--- db/migrate/20130601165029_create_activities.rb
+++ db/migrate/20130601165029_create_activities.rb
@@ -1,12 +1,13 @@
 class CreateActivities < ActiveRecord::Migration
   def change
     create_table :activities, id: :uuid do |t|
-      t.timestamps
       t.string :name
       t.datetime :start_time
       t.datetime :end_time
       t.integer :return_buffer_hours
       t.boolean :completed
+
+      t.timestamps
     end
   end
 end
