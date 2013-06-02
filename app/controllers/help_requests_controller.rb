24ef016169d36ba35ac59dd92c495b702d595f32
Tom Chapin <tchapin@gmail.com>
2013-06-01 19:12:55 -0600

    Removing unnecessary bits from help_requests

--- app/controllers/help_requests_controller.rb
+++ app/controllers/help_requests_controller.rb
@@ -1,66 +1,11 @@
 class HelpRequestsController < ApplicationController
-  before_action :set_help_request, only: [:show, :edit, :update, :destroy]
-
-  # GET /help_requests
-  # GET /help_requests.json
-  def index
-    @help_requests = HelpRequest.all
-  end
+  before_action :set_help_request, only: [:show]
 
   # GET /help_requests/1
   # GET /help_requests/1.json
   def show
     respond_to do |format|
       format.html { render action: 'show', layout: 'maps'}
-    end
-  end
-
-  # GET /help_requests/new
-  def new
-    @help_request = HelpRequest.new
-  end
-
-  # GET /help_requests/1/edit
-  def edit
-  end
-
-  # POST /help_requests
-  # POST /help_requests.json
-  def create
-    @help_request = HelpRequest.new(help_request_params)
-
-    respond_to do |format|
-      if @help_request.save
-        format.html { redirect_to @help_request, notice: 'Help request was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @help_request }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @help_request.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /help_requests/1
-  # PATCH/PUT /help_requests/1.json
-  def update
-    respond_to do |format|
-      if @help_request.update(help_request_params)
-        format.html { redirect_to @help_request, notice: 'Help request was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @help_request.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /help_requests/1
-  # DELETE /help_requests/1.json
-  def destroy
-    @help_request.destroy
-    respond_to do |format|
-      format.html { redirect_to help_requests_url }
-      format.json { head :no_content }
     end
   end
 
