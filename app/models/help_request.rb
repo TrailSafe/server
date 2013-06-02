d4c0d423c2ebce10176bdfe5c0f783ca06d509e9
Jason Waldrip <jason@waldrip.net>
2013-06-02 00:13:02 -0600

    help request changes

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,3 +1,17 @@
 class HelpRequest < ActiveRecord::Base
   belongs_to :device
+
+  before_create :generate_short_url!
+
+  def been_rescued!
+    update_attributes rescued_at: Time.now
+  end
+
+  private
+
+  def generate_short_url!
+    generated = SecureRandom.hex 5
+    self.short_url = self.class.where(short_url: generated).exists? ? generate_short_url! : generated
+  end
+
 end
--- db/migrate/20130601203350_create_help_requests.rb
+++ db/migrate/20130601203350_create_help_requests.rb
@@ -3,12 +3,14 @@ class CreateHelpRequests < ActiveRecord::Migration
     create_table :help_requests, id: :uuid do |t|
       t.uuid :device_id
       t.string :short_url
-      t.boolean :emergency_contact_notified
-      t.boolean :emergency_services_notified
+      t.datetime :user_notified_at
+      t.datetime :emergency_contact_notified_at
+      t.datetime :emergency_services_notified_at
       t.datetime :rescued_at
 
       t.timestamps
 
+      t.index :user_notified_at
       t.index :emergency_contact_notified
       t.index :emergency_services_notified
       t.index :rescued_at
