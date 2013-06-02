39e1bdb89689bcfe18fe021167bae0dc8dd1ca96
Jason Waldrip <jason@waldrip.net>
2013-06-01 20:00:50 -0600

    another attempt to fix deploy

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+Dotenv.load if defined? Dotenv
+
 module Server
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- config/boot.rb
+++ config/boot.rb
@@ -2,10 +2,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
-
-begin
-  require 'dotenv'
-  Dotenv.load
-ensure
-  nil
-end
