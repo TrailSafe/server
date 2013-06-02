7c643af02b8239fe4cfe6a2c92d465e3371da700
Jason Waldrip <jason@waldrip.net>
2013-06-01 19:47:48 -0600

    Rescue form dumb gems

--- config/boot.rb
+++ config/boot.rb
@@ -3,6 +3,9 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-if (require 'dotenv' rescue nil)
+begin
+  require 'dotenv'
   Dotenv.load
+ensure
+  nil
 end
