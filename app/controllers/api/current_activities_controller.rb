287abdf695bb975be2465e67655791a1721420ad
Jason Waldrip <jason@waldrip.net>
2013-06-02 01:23:21 -0600

    current_activities more

---
+++ app/controllers/api/current_activities_controller.rb
@@ -0,0 +1,16 @@
+class Api::CurrentActivitiesController < Api::ApplicationController
+
+  def show
+    @activity = current_device.activities.incomplete.last
+    render 'api/activities/show'
+  end
+
+  def destroy
+    if current_device.activities.incomplete.last.delete
+      head :ok
+    else
+      head 400
+    end
+  end
+
+end
