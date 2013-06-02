3b52b3443ce9fc246cd1293cf4b6c7a0c54a4326
Jason Waldrip <jason@waldrip.net>
2013-06-02 01:17:18 -0600

    help requests changes

--- app/controllers/api/help_requests_controller.rb
+++ app/controllers/api/help_requests_controller.rb
@@ -1,4 +1,7 @@
 class Api::HelpRequestsController < Api::ApplicationController
+
+  before_filter :find_help_request, only: [:destroy, :show]
+
   def create
     if (@help_request = current_device.help_requests.create)
       render :show, status: :created
@@ -8,6 +11,20 @@ class Api::HelpRequestsController < Api::ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    if @help_request.destroy
+      head :ok
+    else
+      head 400
+    end
+  end
+
+  private
+
+  def find_help_request
     @help_request = current_device.help_requests.find params.delete(:id)
   end
+
 end
