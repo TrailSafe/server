68d40fd2fb3f2dc48c3f93ad789c22aba7b94d07
Jason Waldrip <jason@waldrip.net>
2013-06-02 04:00:18 -0600

    changes to help request

--- app/models/help_request.rb
+++ app/models/help_request.rb
@@ -1,18 +1,26 @@
 class HelpRequest < ActiveRecord::Base
-  belongs_to :device, polymorphic: true
+  delegate :current_activity, to: :device
 
-  delegate :current_activity, to: :device, prefix: true
-
+  belongs_to :device
   has_one :user, through: :device
   has_one :emergency_contact, through: :device
 
   before_create :generate_short_url!
+  after_create ->{ alert! :user }
 
   def been_rescued!
     update_attributes rescued_at: Time.now
   end
 
+  def alert!(alertable)
+    alerter_class(alertable).new(self).invoke!
+  end
+
   private
+
+  def alerter_class(alertable)
+    [alertable.to_s, 'alerter'].join('_').classify.constantize
+  end
 
   def generate_short_url!
     generated = SecureRandom.hex 5
