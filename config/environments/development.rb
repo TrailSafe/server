3b1fb61f05ef4322cc5e1e0aba61c361f7f53829
Tom Chapin <tchapin@gmail.com>
2013-06-01 22:32:48 -0600

    Fixing issue with maps.css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Server::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.assets.precompile += %w(*.css *.png *.jpg *.jpeg *.gif)
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
