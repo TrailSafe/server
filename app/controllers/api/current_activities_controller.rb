76755be0637d98396ea72d636d867a38e706545c
Jason Waldrip <jason@waldrip.net>
2013-06-02 01:37:14 -0600

    raise not found errors when a current activity does not exist

--- app/controllers/api/current_activities_controller.rb
+++ app/controllers/api/current_activities_controller.rb
@@ -1,12 +1,12 @@
 class Api::CurrentActivitiesController < Api::ApplicationController
 
   def show
-    @activity = current_device.activities.incomplete.last
+    @activity = current_device.activities.incomplete.last!
     render 'api/activities/show'
   end
 
   def destroy
-    if current_device.activities.incomplete.last.delete
+    if current_device.activities.incomplete.last!.delete
       head :ok
     else
       head 400
