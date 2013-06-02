0138ed5c7c642cb654bb50da184e88a5351683a3
Jason Waldrip <jason@waldrip.net>
2013-06-01 18:59:34 -0600

    activity additions

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,8 @@ class Activity < ActiveRecord::Base
 
   attr_reader :status
 
+  delegate :data, :data=, to: :location_area, prefix: true, allow_nil: true
+
   default_scope -> { order(:updated_at) }
 
   belongs_to :device
@@ -14,20 +16,8 @@ class Activity < ActiveRecord::Base
 
   after_create :set_user_from_device
 
-  def device_has_returned?
-    return_area.contains_device? device
-  end
-
   def duration=(hours)
     self.end_time = Time.now + hours.hours
-  end
-
-  def in_return_time_range?
-    return_time_range.cover? Time.now
-  end
-
-  def in_progress?
-    Time.now <= return_time_range.last
   end
 
   def status
@@ -44,6 +34,28 @@ class Activity < ActiveRecord::Base
     end
   end
 
+  def time_remaining
+    self.end_time - Time.now
+  end
+
+  private
+
+  def device_has_returned?
+    return_area.contains_device? device
+  end
+
+  def in_return_time_range?
+    return_time_range.cover? Time.now
+  end
+
+  def in_progress?
+    Time.now <= return_time_range.last
+  end
+
+  def set_user_from_device
+    self.user = device.user
+  end
+
   def return_time_range
     start_time = end_time - 1.hour
     end_time   = end_time + 1.hour
@@ -52,12 +64,6 @@ class Activity < ActiveRecord::Base
 
   def time_has_elapsed?
     !in_progress?
-  end
-
-  private
-
-  def set_user_from_device
-    self.user = device.user
   end
 
 end
