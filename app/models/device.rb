aeed83a990e51cd3e4e785b6231a1db48c8cf0c5
Jason Waldrip & Tom Chapin <jason+tom@trailsafeapp.com>
2013-06-18 22:31:00 -0600

    Validate device has a proper UUID

--- app/models/device.rb
+++ app/models/device.rb
@@ -10,7 +10,14 @@ class Device < ActiveRecord::Base
   belongs_to :user
 
   # Validations
-  validates :uuid, presence: true, uniqueness: true
+  validates :uuid,
+            presence: true,
+            uniqueness: true,
+            format: {
+              with: /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+              message: 'is not a properly formatted uuid',
+              allow_nil: true
+            }
 
   # Callbacks
   after_create :send_verification_text
